68560aa239e51c3e906bbb904442a8a6fee5bdab
Arinda Clever <48243275+cleverclope@users.noreply.github.com>
2021-12-19 23:28:28 +0300

    changed endpoint baseUrl

--- src/app/Networks/network_config.ts
+++
@@ -1,2 +0,0 @@
-
-export const IP_ADRESS_PORT:string = "http://localhost:1000/erpRealmAPI/1.0/"
--- src/app/_services/signup-user.service.ts
+++ src/app/_services/signup-user.service.ts
@@ -1,20 +1,23 @@
+import { EndPointsService } from 'src/app/_services/endpoints.service';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import SignUpUser from '../models/signup-user';
-import { IP_ADRESS_PORT } from 'src/app/Networks/network_config';
+// import { IP_ADRESS_PORT } from 'src/app/Networks/network_config';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SignupUserService {
 
-  BASE_URL = `${IP_ADRESS_PORT}`
+  // BASE_URL = `${IP_ADRESS_PORT}`
 
-  constructor( private _http: HttpClient) { }
+  constructor( private _http: HttpClient,
+    private endpoints: EndPointsService) { }
 
   public saveClient(postData: SignUpUser) {
-    let endPoint = "CreateUserAccount" 
-    this._http.post(this.BASE_URL + endPoint, postData).subscribe(data => {
+    
+    let endPoint = "CreateUserAccount"
+    this._http.post(this.endpoints.AuthEndpoint(), postData).subscribe(data => {
       console.log(data);
     });
   }
